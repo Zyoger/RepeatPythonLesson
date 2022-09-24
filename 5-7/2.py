5caa5bdadb87aee7698aeeb3f72dff91a7fe09dc
zyoge <zyoger@gmail.com>
2022-09-24 22:24:20 +0300

    add

--- 5-7/2.py
+++ 5-7/2.py
@@ -2,18 +2,21 @@ matrix = [[1, 0, 0, 0],
           [0, 1, 0, 0],
           [0, 0, 1, 0],
           [0, 0, 0, 1]]
-flag = True
+flag = False
 for i in range(0, len(matrix)):
     for j in range(0, len(matrix[i])):
         if i == j:
             if matrix[i][j] != 1:
-                flag = False
+                flag = True
+                print("Не единичная")
                 break
         else:
             if matrix[i][j] != 0:
-                flag = False
+                flag = True
+                print("Не единичная")
                 break
-if flag:
-    print("Единичная")
+    if flag:
+        break
 else:
-    print("Не единичная")
+    print("Единичная")
+

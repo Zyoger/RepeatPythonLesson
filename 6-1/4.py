82f3f470b787253caecd2a73a3e8fed42c81b1b5
zyoge <zyoger@gmail.com>
2022-10-17 22:16:30 +0300

    add

--- 6-1/4.py
+++ 6-1/4.py
@@ -1,1 +1,27 @@
 """4. Дан диапазон целых чисел от n1 до n2. Найти факториал каждого третьего простого числа в заданном диапазоне. """
+
+
+def prime_number(x):
+    """*****"""
+    for divider in range(2, x-1):
+        if x % divider == 0:
+            return False
+    return True
+
+
+def factorial(x):
+    """*****"""
+    res = 1
+    for num in range(2, x+1):
+        res *= num
+    return res
+
+
+i = 4
+j = 11
+count = 0
+for number in range(i, j+1):
+    if prime_number(number):
+        count += 1
+        if count % 3 == 0:
+            print(factorial(number))
--- 6-1/5.py
+++ 6-1/5.py
@@ -1,2 +1,22 @@
 """5. Дано натуральное число N. Уменьшить число в 2 раза (деление нацело) и проверить, изменилось ли после уменьшения
  количество разрядов в числе."""
+
+
+def divider(x):
+    """*****"""
+    if number_digits(x // 2) != number_digits(x):
+        return True
+    else:
+        return False
+
+
+def number_digits(x):
+    """*****"""
+    counter = 0
+    while x > 0:
+        counter += 1
+        x = x // 10
+    return counter
+
+
+print(divider(101))
